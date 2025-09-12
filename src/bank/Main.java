f7a58c4ab3232549023d64a9d2f9b4cda3d46cfb
RaniTayade <ranitayade718@gmail.com>
2025-09-12 10:54:13 +0530

    Add main menu with options

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,7 +1,37 @@
 package bank;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Bank Management System!");
+        Scanner sc = new Scanner(System.in);
+        AccountDAO dao = new AccountDAO();
+
+        while (true) {
+            System.out.println("\n=== Bank Menu ===");
+            System.out.println("1. Create Account");
+            System.out.println("2. View All Accounts");
+            System.out.println("3. Exit");
+            System.out.print("Choose: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Initial Balance: ");
+                    double bal = sc.nextDouble();
+                    dao.createAccount(name, bal);
+                    break;
+                case 2:
+                    dao.getAllAccounts().forEach(System.out::println);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
     }
 }
